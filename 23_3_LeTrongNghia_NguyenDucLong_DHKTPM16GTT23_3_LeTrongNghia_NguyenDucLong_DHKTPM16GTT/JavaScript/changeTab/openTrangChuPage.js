4591410d774c0ca3b7aae023faad6939ec68f2ee
LeeTrongjNghiax <57098216+LeeTrongjNghiax@users.noreply.github.com>
2022-04-21 19:11:25 +0700

    Delete 23_3_LeTrongNghia_NguyenDucLong_DHKTPM16GTT23_3_LeTrongNghia_NguyenDucLong_DHKTPM16GTT directory

--- 23_3_LeTrongNghia_NguyenDucLong_DHKTPM16GTT23_3_LeTrongNghia_NguyenDucLong_DHKTPM16GTT/JavaScript/changeTab/openTrangChuPage.js
+++
@@ -1,6 +0,0 @@
-openTrangChuPage = () => {
-  sessionStorage.setItem("userArray", JSON.stringify(users))
-  sessionStorage.setItem("phoneArray", JSON.stringify(phones))
-
-  window.open("trangChuPage.html")
-}
